5c934a23f77e8b6f337c2906ef93c6fd5a049850
Filipe Brito <43026464+fnbrito@users.noreply.github.com>
2024-03-30 14:48:21 -0400

    Melhoria do movimento
    Movimento está mais melhor :)

--- assets/character/character.gd
+++ assets/character/character.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
-var spd : int = 10
+var spd : int = 200
 var jump_spd : int = 500
 var gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
 var height : int = 450
+var weight : float = 1.5
 
 func _physics_process(delta):
 	# velocidade do jogador
@@ -11,32 +12,32 @@ func _physics_process(delta):
 	velocity.x = spd
 	
 	# pra direita
-	if Input.is_physical_key_pressed(KEY_D):
+	if Input.is_action_pressed("right"):
 		# se a tecla estiver pressionada, aumenta SPEED direita em incrementos de 5 por tick até 200, 
 		# então para em 200
-		spd += 20
-		if spd >= 200:
-			spd = 200
+		spd += 50
+		if spd >= 300:
+			spd = 300
 	
 	# pra esquerda
-	if Input.is_physical_key_pressed(KEY_A):
+	if Input.is_action_pressed("left"):
 		# se a tecla estiver pressionada, aumenta SPEED esquerda em incrementos de 5 por tick até 200,
 		# então para em 200 (é negativo porque move pra esquerda)
-		spd -= 20
-		if spd <= -200:
-			spd = -200
+		spd -= 50
+		if spd <= -300:
+			spd = -300
 			
-	if not Input.is_physical_key_pressed(KEY_A) && spd < 0:
+	if not Input.is_action_pressed("left") && spd < 0:
 		# Se a tecla NÃO estiver sendo pressionada e o valor é menor que 0 (para prevenir 
 		# ativação enquando move para direita), diminui velocidade por 6 por tick
-		spd += 10
+		spd += 20
 			
-	if not Input.is_physical_key_pressed(KEY_D) && spd > 0:
+	if not Input.is_action_pressed("right") && spd > 0:
 		# Se a tecla NÃO estiver sendo pressionada e o valor é maior que 0 (para prevenir 
 		# ativação enquando move para esquerda), diminui velocidade por 6 por tick
-		spd -= 10
+		spd -= 20
 			
-	if spd < 8 && spd > -8 && not Input.is_physical_key_pressed(KEY_A) && not Input.is_physical_key_pressed(KEY_D):
+	if spd < 10 && spd > -10 && not Input.is_action_pressed("left") && not Input.is_action_pressed("move_right"):
 		# Se spd estiver entre -4 e 4 e nenhum botao pressionado, spd = 0
 		# (prevenir movimento se nada estiver sendo pressionado)
 		spd = 0
@@ -44,7 +45,7 @@ func _physics_process(delta):
 	# gravidade
 	if not is_on_floor():
 		# se não é um pulo
-		velocity.y += gravity * delta
+		velocity.y += gravity * delta * weight
 	
 	# pulo
 	if Input.is_action_just_pressed("jump") && is_on_floor():
